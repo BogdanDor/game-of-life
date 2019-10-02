13f9e35ff77cb94fe57d4775c0c4c6e0f90fb6f3
Bogdan Doroschenko <bogdandoroschenko@gmail.com>
2019-10-02 23:58:29 +0300

    draw empty field

--- script.js
+++ script.js
@@ -11,8 +11,39 @@ document.onreadystatechange = function() {
 }
 
 function main() {
-  let fieldCanvas = document.getElementById('field');
-  let fieldCtx = fieldCanvas.getContext('2d');
-  fieldCtx.fillStyle = 'rgb(0, 0, 200)';
-  fieldCtx.fillRect(0, 0, fieldCanvas.clientWidth, fieldCanvas.clientWidth);
+  const fieldCanvas = document.getElementById('field');
+  const fieldCtx = fieldCanvas.getContext('2d');
+  const cellWidth = 10;
+  const lineWidth = 1;
+  const fieldWidth = Math.floor((fieldCanvas.clientWidth - lineWidth) / (lineWidth + cellWidth));
+  const fieldHeight = Math.floor((fieldCanvas.clientHeight - lineWidth) / (lineWidth + cellWidth));
+  drawField(fieldWidth, fieldHeight, cellWidth, lineWidth, fieldCtx);
+}
+
+function drawField(fieldWidth, fieldHeight, cellWidth, lineWidth, context) {
+  context.strokeStyle = '#757472';
+  context.lineWidth = lineWidth;
+  context.beginPath();
+  drawVerticalLines();
+  drawHorizontalLines();
+  context.closePath();
+  context.stroke();
+
+  function drawVerticalLines() {
+    let height = (lineWidth + cellWidth) * fieldWidth + lineWidth;
+    for (let i=0; i<=fieldWidth; i++) {
+      let x = (lineWidth + cellWidth) * i + lineWidth/2;
+      context.moveTo(x, 0);
+      context.lineTo(x, height);
+    }
+  }
+  
+  function drawHorizontalLines() {
+    let width = (lineWidth + cellWidth) * fieldHeight + lineWidth;
+    for (let i=0; i<=fieldHeight; i++) {
+      let y = (lineWidth + cellWidth) * i + lineWidth/2;
+      context.moveTo(0, y);
+      context.lineTo(width, y);
+    }
+  }
 }
